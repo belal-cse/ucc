c42ba7e866e1157a200f18341e2789e7c1a2a6b1
md belal ahmad <ahmadmdbelal991@gmail.com>
2025-11-03 13:59:28 +0530

    update

---
+++ src/Student.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+function Student(){
+
+
+    return(
+        <div className="container ">
+            <div card1>
+                <input type="text" placeholder="Enter username">/</input>
+            </div>
+        </div>
+    );
+}
